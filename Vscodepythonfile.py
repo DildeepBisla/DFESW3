4b099d2cac4d7aed4d4b40f26544998069bffeac
Dildeep Bisla <dildeepbisla2020@gmail.com>
2021-10-01 12:20:02 +0100

    Created Welclome Message Code

--- Vscodepythonfile.py
+++ Vscodepythonfile.py
@@ -1,4 +1,31 @@
-print("I can python")
+#print("I can python")
+
+#print("Hello world".split())
+
+#print("Squirrel, Guinea Pig, Donkey".split(", "))
+
+#print("".join(["Hello", "world", "Your", "Welcome"]))
+
+age = 25
+#print(f"John is {age} years old")
+
+
+
+variable1= "Hello"
+variable2= "World!"
+variable3= "Welcome"
+
+message= variable1 + " " + variable2  
+
+#name = input ("Entername: ")
+
+print(message)
+
+name = input ("Entername: ")
+
+print (message + " " + "Welcome" + " " + name + "!")
+
+
 
 
 
