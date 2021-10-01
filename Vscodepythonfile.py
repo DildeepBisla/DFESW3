b75b0f1b60af0c69f353d06c833b38e0946188a0
Dildeep Bisla <dildeepbisla2020@gmail.com>
2021-10-01 13:13:33 +0100

    Added Input and boolean exercise

--- Vscodepythonfile.py
+++ Vscodepythonfile.py
@@ -1,30 +1,67 @@
 #print("I can python")
 
+#\
+
 #print("Hello world".split())
+
+#\
 
 #print("Squirrel, Guinea Pig, Donkey".split(", "))
 
+#\
+
 #print("".join(["Hello", "world", "Your", "Welcome"]))
 
-age = 25
-#print(f"John is {age} years old")
+#\
+
+# age = 25
+# print("John is {age} years old")
+
+# age = 25
+# print(f"John is {age} years old")
 
 
+#\
 
-variable1= "Hello"
-variable2= "World!"
-variable3= "Welcome"
+#variable1= "Hello"
+#variable2= "World!"
+#variable3= "Welcome"
 
-message= variable1 + " " + variable2  
+#message= variable1 + " " + variable2  
 
 #name = input ("Entername: ")
 
-print(message)
+#print(message)
 
-name = input ("Entername: ")
+#name = input ("Entername: ")
 
-print (message + " " + "Welcome" + " " + name + "!")
+#print (message + " " + "Welcome" + " " + name + "!")
 
+#\
+
+#number1 = input("Enter whole number: ")
+#number2 = input("Enter decimal number: ")
+
+#integer_number = int(number1)
+#float_number = float(number2)
+#round_number = int(round(float_number))
+
+#print(number1)
+#print(number2)
+#print(round_number)
+
+ #\
+
+# name= "Dillon Bisla"
+# age= "33"
+# male= True
+# female= False
+
+# print ( "My name is " + name + " I am " + age + " years old")
+# print ( "Statement: ", name, "my gender is male. ", male)
+# print ( "Statement: ", name, "my gender is female. ", female)
+
+#\
 
 
 
