429d8496c3a4382020343057112f05cc028bf2a0
Dildeep Bisla <dildeepbisla2020@gmail.com>
2021-10-04 14:29:28 +0100

    While Loops

--- conditionals.py
+++ conditionals.py
@@ -46,7 +46,12 @@
 #     else:
 #         print('This is true')
 
+
+
 #\\\
+
+
+
 
 # stringVar = 'this is a string'
 
@@ -57,14 +62,25 @@
 #     else:
 #         print('This is not')
 
-#\
 
-for numVar in  range(0, 100, 7):
-    print(numVar)
+
+#\\\
+
+
+
+
+# for numVar in  range(0, 100, 7):
+#     print(numVar)
 
 #\ ^ used in a 7 times table format
 
-#\
+
+
+
+
+#\\\
+
+
 
 # my_fruit = ["Apple", "Banana", "Orange", "Pear", "Grape", "Berry"]
 
@@ -73,5 +89,88 @@ for numVar in  range(0, 100, 7):
 #     print(fruit.upper())
 
 
+
+
+
+
+
+
+#\\\
+
 # WHILE LOOPS
+
+# inputNumbVar = int(input('Enter NUmber: '))
+
+# resultVar = 1
+
+# while inputNumbVar > 0:
+#     resultVar= resultVar * inputNumbVar
+#     inputNumbVar = inputNumbVar - 1
+#     print(resultVar)
+
+
+
+#\
+
+
+# userinput = int(input('Enter Number: '))
+
+# usercode =+ 3
+
+# while userinput > 0:
+#     userscore = usercode * userinput
+#     print(userscore)
+#     break
+
+#\
+
+# for i in range(3):
+#     for j in range(4):
+#         print(i, "x", j, "=", i * j)
+
+
+#\
+
+
+# count = 0
+# name = str(input("What is your name? "))
+
+# while count < 5:
+#     print(name, "is awesome! ")
+#     count += 1
+
+
+#\
+
+
+# for i in range(5, 11):
+#     print(i)
+
+
+#\
+#
+#
+
+
+# while True:
+#     name = str(input('Entername: '))
+#     print(name + " " + "is awesome!")
+    
+#
+#\
+#
+
+
+# names = 0
+
+# while names < 5:
+
+#     name = input("Please enter a name: ")
+
+#     print(name + " Is awesome!")
+
+#     names = names + 1
+
+
+
 
