86b112ff52a0e67f10ecf70182142a1578409f0c
Dildeep Bisla <dildeepbisla2020@gmail.com>
2021-10-04 11:47:12 +0100

    LOOPS ADDED

--- conditionals.py
+++ conditionals.py
@@ -1,12 +1,70 @@
 
+#\
 
-devs_money = 100
-dev_can_play_smash = False
+# devs_money = 100
+# dev_can_play_smash = False
 
-if devs_money > 10 and dev_can_play_smash:
-    print("Dev enters a smash tournament!")
+# if devs_money > 10 and dev_can_play_smash:
+#     print("Dev enters a smash tournament!")
 
-elif devs_money < 10 and dev_can_play_smash:
-    print("Dev is too poor to enter")    
+# elif devs_money < 10 and dev_can_play_smash:
+#     print("Dev is too poor to enter")    
 
-    
+# else:
+#     print("Dev just can't play smash")
+
+
+#\
+
+# usermark = int(input("Enter Mark: "))
+
+# if usermark >= 85:
+#     print("Distinction") 
+
+# elif usermark >= 65 and usermark < 85:
+#     print("pass")
+
+# else: 
+#     print ("fail")
+
+#\
+
+
+
+
+
+#\ LOOPS
+
+# FOR LOOPS
+
+# stringVar = '011100010010'
+
+# for intvar in stringVar:
+#     if int(intvar):
+#         print('this is false')
+
+#     else:
+#         print('This is true')
+
+#\\\
+
+# stringVar = 'this is a string'
+
+# for teststringvar in stringVar:
+#     if teststringvar == 't':
+#         print('this is a TEEEE')
+
+#     else:
+#         print('This is not')
+
+#\
+
+my_fruit = ["Apple", "Banana", "Orange", "Pear", "Grape", "Berry"]
+
+for fruit in my_fruit:
+    # add if statements here (optional)
+    print(fruit.upper())
+
+
+# WHILE LOOPS
+
