28010d791c4aaf09564f76b431a3c9e0273435f2
Dildeep Bisla <dildeepbisla2020@gmail.com>
2021-10-06 10:33:47 +0100

    Exercise 1

--- debugging.py
+++ debugging.py
@@ -1,10 +1,11 @@
 
+
 user_funds = 10.31
 item_price = price["Burger"]
 
 if item_price < user_funds:
-    Print("You have enough money!")
-if item_price = user_funds:
+    print("You have enough money!")
+if item_price == user_funds:
     print("You have the precise amount of money")
 if item_price < user_funds:
-    print(Sorry you don't have enough money)
+    print("Sorry you don't have enough money")
