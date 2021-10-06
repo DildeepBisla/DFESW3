536c5a6c4beee6f05e2bf44d18d0458882016fda
Dildeep Bisla <dildeepbisla2020@gmail.com>
2021-10-06 18:19:59 +0100

    list methods added

---
+++ Lists CS Dojo.py
@@ -0,0 +1,32 @@
+# #created a list
+# a = [3, 10, -1]
+# print (a)
+
+# #append list
+# a.append (1)
+# print (a)
+
+# #delete items in list
+# a.pop (1)
+# print (a)
+
+# #index number
+# a [0]
+# print(a[0])     # prints number 3 and so on
+
+b = ["banana", "apple", "microsoft"]
+
+i = b.index("banana")
+print(i)           # so banana is at index 0
+
+i = b.index("microsoft")
+print(i)           # so microsoft is at index 2
+
+i = b.count("banana")
+print(i)           # need to specify the item that is required for "count()" in the ()
+
+print(len("apple")) # prints the length of the string/int
+print(len(b))       # prints the length of the string/int
+
+print(list(b))      #shows the items in the list or array
+
