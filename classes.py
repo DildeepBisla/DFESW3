ea84c198202990d47d75cd93f009d556ecdd3f69
Dildeep Bisla <dildeepbisla2020@gmail.com>
2021-10-06 14:31:36 +0100

    tutorial

---
+++ classes.py
@@ -0,0 +1,50 @@
+# Tutorial
+
+# class Student:
+
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# John = Student("John", "21")
+# Jane = Student("Jane", "22")
+
+# print(getattr(John, "age"))
+
+####
+
+
+class Student:
+
+    def __init__(self, name, age):
+            self.name = name
+            self.age = age
+            self.class_ = "student"
+
+
+    def test_scores(self, res1, res2, res3):
+        return (res1 + res2 + res3) / 3
+
+res1 = 32
+res2 = 25
+res3 = 55
+
+print()
+
+
+
+
+
+
+# name = (input("Student Name: "))
+# age = (input("Student age: "))
+# class_year = (input("Student Class Year: "))
+
+
+# Res1 = int(input("Enter Res1: "))
+# Res2 = int(input("Enter Res2: "))
+# Res3 = int(input("Enter Res3: "))
+
+# dillon = Student("Dillon", 33, "QA Y1")
+
+# print(dillon)
