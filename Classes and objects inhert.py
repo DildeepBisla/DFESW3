6966c02481f72933df6fc2d595f7e85b948c2704
Dildeep Bisla <dildeepbisla2020@gmail.com>
2021-10-07 13:18:38 +0100

    Max of three numbers func

--- Classes and objects inhert.py
+++ Classes and objects inhert.py
@@ -1,1 +1,33 @@
 
+class Dog(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def speak(self):
+        print("HI iam ", self.name, "and i am", self.age, 'years old')
+
+    def talk(self):
+        print('Bark!')
+
+# Dog is superclass
+
+class Cat(Dog):
+    def __init__(self, name, age, color):
+        super().__init__(name, age)     #<--- auto inherit from Dog because of (Dog)
+        self.color = color
+
+    def talk(self):
+        print('meow')       # whatever goes in the subclass it will override the DOG 
+                            # class
+
+
+tim = Cat('tim', 5, 'blue')        
+tim.speak()
+
+jim = Dog('jim', 70)
+jim.talk()
+
+
+
+
---
+++ function tasks.py
@@ -0,0 +1,24 @@
+#Find the MAX of three numbers
+
+# def max_of_three(z,y,x):
+         
+#     if z > y and z > x :
+#         print("Z is greater")
+    
+#     elif y > z and y > x:
+#        print("Y is greater")
+    
+#     elif x > z and x > y:
+#          print("X is greater")
+    
+#     else:
+#         print("not res is greater")
+
+# max_of_three(1, 10, 20)       
+# max_of_three(20,10,1)
+# max_of_three(10, 30, 20)
+
+
+# task 2
+
+
