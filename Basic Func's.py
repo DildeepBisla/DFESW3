5674ce566df00550ed106f64d65a27ab1099b2e8
Dildeep Bisla <dildeepbisla2020@gmail.com>
2021-10-07 11:27:21 +0100

    07sept changes

--- Basic Func's.py
+++ Basic Func's.py
@@ -1,1 +1,17 @@
 
+numb_list = [1,2,3,4,5,6,7,8,9,10]
+
+x = range(5)
+for x in numb_list:
+    print(x)
+
+          # in this method x variable is going represent the range in the numb_list
+          # and pull the numbers till it gets to 9 while (-1) going down 1 
+
+# x = range(6)        #<---- range is to 6 (no list used just range is auto generated) 
+# for n in x:         #<---- n is just letter assinged to the iteration in the range 
+#   print(n) 
+
+#         # so x is the varibale, its going to have a range of auto populated iteration 
+#         # to 6.  n is going to pull the iteration in x till it hit 6 iteration.  
+
