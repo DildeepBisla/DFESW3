90d0b86559c0815c9055f532817b833f3ef17b01
Dildeep Bisla <dildeepbisla2020@gmail.com>
2021-10-07 12:10:22 +0100

    changes

--- Lists CS Dojo.py
+++ Lists CS Dojo.py
@@ -53,5 +53,27 @@
 # = list(stringvariable) <---- to convert string into list
 
 
+#######             RANGE               ##########
+
+# numb_list = [1,2,3,4,5,6,7,8,9,10]
+
+# x = numb_list[3:7]
+# print(x)
+
+          # in this method x variable is going represent the range in the numb_list
+          # and pull the numbers from index 3 to 7 
+
+# x = range(6)        #<---- range is to 6 (no list used just range is auto generated) 
+# for n in x:         #<---- n is just letter assinged to the iteration in the range 
+#   print(n) 
+
+#         # so x is the varibale, its going to have a range of auto populated iteration 
+#         # to 6.  n is going to pull the iteration in x till it hit 6 iteration.  
 
 
+
+######      Making Dictionaries         #######
+
+#               Dictionaries are defined using { : }, as shown below.
+
+#       noises = {"cow" : "moo", "sheep" : "baa"}
--- Roman Num Converter.py
+++ Roman Num Converter.py
@@ -1,16 +1,1 @@
 
-# class Numurail():
-
-#     def converter(self, rom, num, output_num):
-#         self.rom = rom
-#         self.num = num
-#         self.output_num = output_num
-
-       
-
-
-# num = [1, 4, 5, 9, 10]
-# rom = ['I', 'IV', 'V', 'IX', 'X']
-
-# num = 1
-
