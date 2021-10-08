26ab1ca1a5695f3bdee784814d563d4da2e71aa9
Dildeep Bisla <dildeepbisla2020@gmail.com>
2021-10-08 13:49:29 +0100

    no changes

--- conditionals.py
+++ conditionals.py
@@ -1,17 +1,17 @@
 
 #\
 
-# devs_money = 100
-# dev_can_play_smash = False
+devs_money = 100
+dev_can_play_smash = False
 
-# if devs_money > 10 and dev_can_play_smash:
-#     print("Dev enters a smash tournament!")
+if devs_money > 10 and dev_can_play_smash:
+    print("Dev enters a smash tournament!")
 
-# elif devs_money < 10 and dev_can_play_smash:
-#     print("Dev is too poor to enter")    
+elif devs_money < 10 and dev_can_play_smash:
+    print("Dev is too poor to enter")    
 
-# else:
-#     print("Dev just can't play smash")
+else:
+    print("Dev just can't play smash")
 
 
 #\
