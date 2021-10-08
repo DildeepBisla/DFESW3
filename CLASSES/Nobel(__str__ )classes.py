6f969d4645736551e18343f3ee9a89991a732560
Dildeep Bisla <dildeepbisla2020@gmail.com>
2021-10-08 14:29:33 +0100

    string in def class method

---
+++ CLASSES/Nobel(__str__ )classes.py
@@ -0,0 +1,19 @@
+
+###### Classes NOBEL ######
+
+# __str__ function can be used to return a string representation for the class when 
+# needed.
+
+
+class Nobel:
+
+    def __init__(self, category, year, winner):
+        self.category = category
+        self.year = year
+        self.winner = winner
+
+    def __str__(self):
+        return "{} was the winner of Nobel {} Prize in {}".format(self.winner, self.category, self.year)
+
+np2005=Nobel("Peace", 2005, "Muhammad Yunus")
+print(np2005)
